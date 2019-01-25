6992483b86a79eb20fd0fc36e74b7949f4e08a6f
AlexMukha <alexmukha@email.com>
2019-01-24 21:12:13 -0800

    Return results successful

--- NYTimes_API/nytime.js
+++ NYTimes_API/nytime.js
@@ -1,9 +1,14 @@
 
 
-$("#run-search").on("click", function(){
+$("#run-search").on("click", function(event){
+// $("form").submit(function(event){
+event.preventDefault();   
+console.log("clicked"); 
     // $("#article-section").append("results");
 var params = {};
-params["api_key"]= "rtv8CmipUMdk5CMdDmwqdtJ6qYmvITrp";
+// params["api-key"]= "b9f91d369ff59547cd47b931d8cbc56b";
+// params["api-key"]= "0pGzGJA8JjzemGwdCdQzV1bghEzLU0gM";
+params["api-key"]= "rtv8CmipUMdk5CMdDmwqdtJ6qYmvITrp";
 params.q = $("#search-term").val().trim();
 // params.begin_date= $("#start-year").val().trim();
 // params.end_date= $("#end-year").val().trim();
@@ -26,6 +31,9 @@ $.ajax({
         // }
 
     })
-  
+//   if (pass)
+//   return true;
+//   else
+//   return false;
 
 })
