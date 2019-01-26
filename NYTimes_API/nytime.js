63ce48dfb0de9e7c45046b5cf6fe88dfbab40492
AlexMukha <alexmukha@email.com>
2019-01-25 22:23:16 -0800

    Working but incomplete

--- NYTimes_API/nytime.js
+++ NYTimes_API/nytime.js
@@ -5,36 +5,39 @@ $("#run-search").on("click", function(event){
 event.preventDefault();   
 console.log("clicked"); 
     // $("#article-section").append("results");
+    var monthDay = "0101";
 var params = {};
 params["api-key"]= "rtv8CmipUMdk5CMdDmwqdtJ6qYmvITrp";
 params.q = $("#search-term").val().trim();
-// params.begin_date= $("#start-year").val().trim();
-// params.end_date= $("#end-year").val().trim();
+var yearStart = $("#start-year").val().trim();
+var yearEnd = $("#end-year").val().trim();
+params.begin_date= yearStart+monthDay;
+params.end_date= yearEnd+monthDay;
 params.limit = $("#article-count").val();
 var paramsStr = $.param(params);
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"+ paramsStr;
-
+console.log(paramsStr);
 
 $.ajax({
     url: queryURL,
     method: "GET"
     })
     .then(function(response) {
-        console.log(response);
-        console.log(response.response);
-        // console.log(response.response.docs);
         var results = response.response.docs;
         for (var i= 0; i < results.length; i++) {
-            console.log(results[i].pub_date);
-            console.log(results[i].byline.original);
-            console.log(results[i].headline.main);
-            console.log(results[i].web_url);
-            console.log(results[i].snippet);
-            $("#article-section").append("<b>Posted: </b> "+results[i].pub_date+"<br>");
-            $("#article-section").append("<b>Written by:</b> "+results[i].byline.original+"<br>");
-            $("#article-section").append("<b>Title: </b><a href='"+results[i].web_url+"'> "+results[i].headline.main+"</a><br>");
-            $("#article-section").append(results[i].snippet+"<br><p>&nbsp;</p>");
+            // console.log(results[i].pub_date);
+            // console.log(results[i].byline.original);
+            // console.log(results[i].headline.main);
+            // console.log(results[i].web_url);
+            // console.log(results[i].snippet);
+            $("#article-section").append("<b>Posted:</b> "+results[i].pub_date+"<br>");
+            // $("#article-section").append("<b>Written:</b> "+results[i].byline.original+"<br>");
+            $("#article-section").append("<b>Written by:</b> "+results[i].byline.person[0].firstname+" "+results[i].byline.person[0].lastname+"<br>");
+            $("#article-section").append("<b>Title:</b>"+results[i].headline.main+"<br>");
+            $("#article-section").append(results[i].snippet+"<br>");
+            $("#article-section").append("<a href='"+results[i].web_url+"'><b>Read more...</b></a><br><p>&nbsp;</p>");
+            
     }
 
     })
