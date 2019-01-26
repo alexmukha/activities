cf78be7ffaf8f05722b366cd945e02948d926172
AlexMukha <alexmukha@email.com>
2019-01-25 21:53:09 -0800

    Working code

--- NYTimes_API/nytime.js
+++ NYTimes_API/nytime.js
@@ -14,8 +14,7 @@ params.limit = $("#article-count").val();
 var paramsStr = $.param(params);
 
 var queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?"+ paramsStr;
-// console.log($("#search-term").val().trim());
-//   console.log(queryURL);
+
 
 $.ajax({
     url: queryURL,
@@ -27,13 +26,15 @@ $.ajax({
         // console.log(response.response.docs);
         var results = response.response.docs;
         for (var i= 0; i < results.length; i++) {
-            console.log(response.response.docs[i].headline.main);
-
-            $("#article-section").append("<p><b>Posted: </b> "+results[i].pub_date+"<br>");
+            console.log(results[i].pub_date);
+            console.log(results[i].byline.original);
+            console.log(results[i].headline.main);
+            console.log(results[i].web_url);
+            console.log(results[i].snippet);
+            $("#article-section").append("<b>Posted: </b> "+results[i].pub_date+"<br>");
             $("#article-section").append("<b>Written by:</b> "+results[i].byline.original+"<br>");
             $("#article-section").append("<b>Title: </b><a href='"+results[i].web_url+"'> "+results[i].headline.main+"</a><br>");
-            $("#article-section").append(results[i].snippet+"<br></p>");
-       
+            $("#article-section").append(results[i].snippet+"<br><p>&nbsp;</p>");
     }
 
     })
